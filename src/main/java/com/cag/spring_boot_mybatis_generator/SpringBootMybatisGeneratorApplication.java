b159407ac34ef26b512743e787eee33612f32fc7
cag2050 <cag2050@163.com>
2017-11-27 18:16:02 +0800

    m

---
+++ src/main/java/com/cag/spring_boot_mybatis_generator/SpringBootMybatisGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.cag.spring_boot_mybatis_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cag/spring_boot_mybatis_generator/SpringBootMybatisGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cag.spring_boot_mybatis_generator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMybatisGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
